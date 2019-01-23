b0a179d88c7cc2992a24434fe05fd591bc5c0235
ConnorNewtonDev <dev@connornewton.co.uk>
2019-01-23 16:50:11 +0000

    .Net(2.0) & SerialPort
    Rebuilt program to use .Net(2.0) so that System.IO.Ports was available for use as issues w/ latest unity version.

---
+++ Zumo/Assets/Scripts/AppHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+public class AppHandler : MonoBehaviour
+{
+    private SerialPort sp;
+
+    void Start()
+    {
+        sp = new SerialPort("COM4", 9600);
+        sp.ReadTimeout = 50;
+        sp.Open();
+    }
+
+#region Buttons
+    public void ForwardBtn()
+    {
+        WriteToSerial("MF");
+    }
+
+    public void LeftBtn()
+    {
+        WriteToSerial("LT");
+    }
+
+    public void RightBtn()
+    {
+        WriteToSerial("RT");
+    }
+#endregion
+
+    void WriteToSerial(string _msg)
+    {
+        sp.WriteLine(_msg);
+        sp.BaseStream.Flush();          //Flush() to ensure all data is sent at the same time
+    }
+}
