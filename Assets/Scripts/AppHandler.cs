fdd3d12a4171b154277262a2e45cab323b7c6a84
ConnorNewtonDev <dev@connornewton.co.uk>
2019-01-23 16:14:50 +0000

    Code foundation
    Basic structure set out for both app and zumo side.

---
+++ Assets/Scripts/AppHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.IO.Ports;
+using UnityEngine;
+
+public class AppHandler : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+#region Buttons
+    void ForwardBtn()
+    {
+
+    }
+
+    void LeftBtn()
+    {
+
+    }
+
+    void RightBtn()
+    {
+
+    }
+#endregion
+
+    void WriteToSerial()
+    {
+        
+    }
+}
---
+++ ZumoCode/zumoSketch/zumoSketch.ino
@@ -0,0 +1,60 @@
+#include <SoftwareSerial.h>
+#include <SerialCommand.h>
+
+SerialCommand serialCmd;
+
+void setup() 
+{
+  Serial.begin(9600);
+  while(!Serial);
+}
+  
+void loop() 
+{
+  if(Serial.available() > 0)    
+    {
+      serialCmd.readSerial();
+      serialHandler();
+    }
+    
+}
+
+void serialHandler()
+{
+  char *arg;
+  arg = serialCmd.next();
+
+  switch(arg)
+  {
+    case "LT":
+    {
+      leftHandler();
+      break;
+    }
+    case "RT":
+    {
+      rightHandler();
+      break;
+    }
+    case "MF":
+    {
+      forwardHandler();
+      break;
+    }
+  }
+  
+}
+void leftHandler()
+{  
+  Serial.println("Left Turn");    
+}
+
+void rightHandler()
+{  
+  Serial.println("Right Turn");
+}
+
+void forwardHandler()
+{
+  Serial.println("Move Forward");
+}
