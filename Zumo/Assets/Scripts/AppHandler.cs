efacd7bee2aa2c3a4bb8d400714674e97cc8a679
ConnorNewtonDev <dev@connornewton.co.uk>
2019-01-28 04:28:14 +0000

    GUI & Zumo Code
    Features are implemented including auto forward, manual control, room scanning

--- Zumo/Assets/Scripts/AppHandler.cs
+++ Zumo/Assets/Scripts/AppHandler.cs
@@ -2,34 +2,155 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO.Ports;
-
+using TMPro;
 public class AppHandler : MonoBehaviour
 {
     private SerialPort sp;
+    private TextMeshProUGUI serialFeed;
 
+    //Axis
+    float dValue = 0;
+    float lValue = 0;
+    float uValue = 0;
+    float rValue = 0;
+
+    //Btn bools
+    bool aDown = false;
+    bool bDown = false;
+    bool xDown = false;
+    bool yDown = false;
+
+    enum ChangeState {NOCHANGE = 0, CHANGETRUE = 1, CHANGEFALSE =2};
+    
     void Start()
     {
-        sp = new SerialPort("COM4", 9600);
+        serialFeed = GameObject.Find("SerialFeed").GetComponent<TextMeshProUGUI>();
+        sp = new SerialPort("COM7", 9600);
         sp.ReadTimeout = 50;
         sp.Open();
     }
 
-#region Buttons
-    public void ForwardBtn()
+    #region ChangeStates
+
+    ChangeState CheckAxis(string axisName, ref float axisDown, int mod)
     {
-        WriteToSerial("MF");
+        if(Input.GetAxis(axisName) != axisDown)
+        {
+            if(Input.GetAxis(axisName) * mod >= 0.3f && axisDown * mod < 1)
+            {
+                axisDown = 1;
+                return ChangeState.CHANGETRUE;
+            }
+            else
+            {
+                axisDown = 0;
+                return ChangeState.CHANGEFALSE;
+            }
+        }
+
+        return ChangeState.NOCHANGE;
     }
 
-    public void LeftBtn()
+    ChangeState CheckBtn(string btnName, ref bool btnDown)
     {
-        WriteToSerial("LT");
-    }
+        if (Input.GetButton(btnName) != btnDown)
+        {
+            if (!btnDown)
+            {
+                btnDown = true;
+                return (ChangeState.CHANGETRUE);
+            }
+            else
+            {
+                btnDown = false;
+                return (ChangeState.CHANGEFALSE);
+            }
 
-    public void RightBtn()
-    {
-        WriteToSerial("RT");
+        }
+        else
+            return (ChangeState.NOCHANGE);
     }
+    #endregion
+
+    void Update()
+    {    
+    #region DPAD
+        ChangeState state = ChangeState.NOCHANGE;
+        state = CheckAxis("dpadY", ref dValue, -1);
+       if(state == ChangeState.CHANGETRUE)
+        {
+            //Send down message
+            sp.Write("b");
+            string temp = "\n Go Back";
+            serialFeed.text = temp;
+        }
+
+        state = CheckAxis("dpadX", ref lValue, -1);
+        if (state == ChangeState.CHANGETRUE)
+        {
+            //Send down message
+            sp.Write("l");
+            string temp = "\n Turn Left";
+            serialFeed.text = temp;
+        }
+
+        state = CheckAxis("dpadY", ref uValue, 1);
+        if (state == ChangeState.CHANGETRUE)
+        {
+            //Send down message
+            sp.Write("f");
+            string temp = "\n Go Forward";
+            serialFeed.text = temp;
+        }
+
+        state = CheckAxis("dpadX", ref rValue, 1);
+        if (state == ChangeState.CHANGETRUE)
+        {
+            //Send down message
+            sp.Write("r");
+            string temp = "\n Turn Right";
+            serialFeed.text = temp;
+        }
 #endregion
+
+    #region Buttons
+        // X button
+        if (CheckBtn("X", ref xDown) == ChangeState.CHANGETRUE)
+        {           
+            sp.Write("o");
+            string temp = "Scan Left";
+            serialFeed.text = temp;
+        }
+        else
+        // B button
+        if (CheckBtn("B", ref bDown) == ChangeState.CHANGETRUE)
+        {
+            sp.Write("p");
+            string temp = "Scan Right";
+            serialFeed.text = temp;
+        }
+        else
+        // Y button
+        if (CheckBtn("Y", ref yDown) == ChangeState.CHANGETRUE)
+        {
+            sp.BaseStream.Flush();
+            sp.Write("i");
+            string temp = "Interrupt";
+            serialFeed.text = temp;
+        }
+        else
+        // A button
+        if (CheckBtn("A", ref aDown) == ChangeState.CHANGETRUE)
+        {
+            sp.Write("a");
+            string temp = "Forward";
+            serialFeed.text = temp;
+        }
+
+
+        #endregion
+    }
+
 
     void WriteToSerial(string _msg)
     {
--- ZumoCode/zumoSketch/zumoSketch.ino
+++ ZumoCode/zumoSketch/zumoSketch.ino
@@ -1,7 +1,16 @@
-#include <SoftwareSerial.h>
-#include <SerialCommand.h>
+#define LED 13
+#define SPEED 100
+#define EDGELIMIT 500 
+#define WALLLIMIT 1500 
+#define TRIGGERPIN  2  // Sensor trigger pin.
+#define ECHOPIN     6  // Sensor echo pin.
+#define DIST 200 
+ZumoBuzzer buzzer;
+ZumoMotors motors;
+NewPing sonar(TRIGGERPIN, ECHOPIN, DIST); // NewPing setup of pins and maximum distance.
 
-SerialCommand serialCmd;
+unsigned int sensors[6];
+ZumoReflectanceSensorArray reflectSensors(QTR_NO_EMITTER_PIN);
 
 void setup() 
 {
@@ -11,52 +20,143 @@ void setup()
   
 void loop() 
 {
+  int msg = 0;
   if(Serial.available() > 0)    
     {
-      serialCmd.readSerial();
-      serialHandler();
+       msg = Serial.read();
+       handleMsg(msg);
     }
     
 }
 
-void serialHandler()
+void handleMsg(int _msg)
 {
-  char *arg;
-  arg = serialCmd.next();
-
-  switch(arg)
+  if(msg == 'f')
   {
-    case "LT":
+    Serial.println("Move Forward");  
+    motors.setSpeeds(SPEED, SPEED);
+  }
+  else if(msg == 'l')
+  {
+    Serial.println("Turn Left");  
+        motors.setSpeeds(-SPEED, SPEED);
+  }
+  else if(msg == 'r')
+  {
+    Serial.println("Turn Right");  
+        motors.setSpeeds(SPEED, -SPEED);
+  }
+  else if(msg == 'b')
+  {
+    Serial.println("Move Back");  
+        motors.setSpeeds(-SPEED, -SPEED);
+  }
+  else if(msg == 's')
+  {
+    Serial.println("Stop");
+        motors.setSpeeds(0, 0);
+  }
+  else if(msg == 'a')
+  {
+    autoForward();
+    Serial.println("Automated");
+  }
+  else if(msg == 'i')
+  {
+    Serial.println("Interrupt");
+  }    
+  else if(msg == 'o')
+  {
+    Serial.println("Scan Left");
+    scanRoom(-1); //Inverse values
+  }  
+  else if(msg == 'p')
+  {
+    scanRoom(1);
+  }
+    Serial.println("Scan Right");
+  }
+}
+
+void autoForward()
+{
+   bool interrupt = false;
+  while (!interrupt)
+  {
+    if(lastCMD == 's')
+      interrupt = true;
+      
+    reflectSensors.read(sensors);
+    
+    float avg = sensors[0] +
+                sensors[1] +
+                sensors[2] +
+                sensors[3] +
+                sensors[4] +
+                sensors[5];
+                
+    avg = avg / 6;
+    
+    if (avg > EDGELIMIT)
+      interrupt = true;
+      
+    else if (sensors[0] > WALLLIMIT)
     {
-      leftHandler();
-      break;
+      // Left Sensors
+      motors.setSpeeds(-SPEED, -SPEED);
+      delay(100);
+      motors.setSpeeds(SPEED, -SPEED);
+      delay(100);
+      motors.setSpeeds(SPEED, SPEED);
     }
-    case "RT":
+    else if (sensors[5] > WALLLIMIT)
     {
-      rightHandler();
-      break;
+      // Right Sensors
+      motors.setSpeeds(-SPEED, -SPEED);
+      delay(100);
+      motors.setSpeeds(-SPEED, SPEED);
+      delay(100);
+      motors.setSpeeds(SPEED, SPEED);
     }
-    case "MF":
+    else
     {
-      forwardHandler();
-      break;
+      //Straight Ahead
+      motors.setSpeeds(SPEED, SPEED);
+      msg = Serial.read();
     }
+
+  }
+}
+
+void searchRoom(int modifier)
+{
+    bool objInRoom = false;
+  
+  motors.setSpeeds(SPEED, SPEED);
+  delay(700);
+  motors.setSpeeds(modifier * SPEED, modifier * -SPEED);
+  delay(1500);
+  motors.setSpeeds(SPEED, SPEED);
+  delay(100);  
+  motors.setSpeeds(modifier * -SPEED, modifier * SPEED);
+
+  for(int i = 0; i < 30; i++)
+  {
+    delay(50);
+    float range = sonar.ping_cm();
+    if(range < 17.5 && range > 2)
+      objInRoom = true;
   }
   
-}
-void leftHandler()
-{  
-  Serial.println("Left Turn");    
-}
+  motors.setSpeeds(mod * SPEED, modifier * -SPEED);
+  for(int i = 0; i < 30; i++)
+  {
+    delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    float range = sonar.ping_cm();
+    if(range < 15 && range > 2)
+      objInRoom = true;
+  }
+  motors.setSpeeds(0, 0);
 
-void rightHandler()
-{  
-  Serial.println("Right Turn");
-}
-
-void forwardHandler()
-{
-  Serial.println("Move Forward");
-  //Check sensor
-  //Adjust if needed
+  
 }
